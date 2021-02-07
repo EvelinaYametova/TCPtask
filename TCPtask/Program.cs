e4d11c59d4c59474866ce53754495d3d265041dd
Evelina <evelinaoct@yandex.ru>
2021-02-07 22:42:32 +0500

    update Program.cs

--- TCPtask/Program.cs
+++ TCPtask/Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Linq;
 
 namespace TCPtask
 {
     class Program
     {
+        static string IPAddress = "127.0.0.1";
         static void Main(string[] args)
         {
             Console.WriteLine("Программа выводит открытые порты");
+            Console.WriteLine("Введите IP адрес");
+            IPAddress = Console.ReadLine();
             Console.WriteLine("Введите два целых числа от 0 до 65536 через пробел(диапазон портов)");
             var ports = Console.ReadLine().Split().Select(p => double.Parse(p)).OrderBy(p => p);
             if (ports.All(p => p >= 0 && p <= 65536 && p % 1 == 0))
@@ -27,7 +30,18 @@ namespace TCPtask
 
         static bool CheckIfPortIsOpen(int port)
         {
-            return IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners().Any(l => l.Port == port);
+            try
+            {
+                using (var tcpClient = new TcpClient())
+                {
+                    tcpClient.Connect(IPAddress, port);
+                    return true;
+                }
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
         }
     }
 }
